0f604ad5dac80517fcb30edad127e540c87a6f6f
pazhosch <d.m.baranov@gmail.com>
2019-04-07 15:18:35 +0300

    Revert "Qualifiers (39)"
    This reverts commit 766a741c

--- src/main/java/guru/springframework/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/controllers/ConstructorInjectedController.java
@@ -3,7 +3,6 @@ package guru.springframework.controllers;
 import guru.springframework.services.GreetingService;
 import guru.springframework.services.GreetingServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 /**
@@ -16,7 +15,7 @@ public class ConstructorInjectedController {
     private GreetingService greetingService;
 
     // @Autowired // automatic wiring since 4.2
-    public ConstructorInjectedController(@Qualifier("constructorGreetingService") GreetingService greetingService) {
+    public ConstructorInjectedController(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
