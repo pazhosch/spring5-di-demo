7470467a044afda8616fd8f6553e4c69b83b2a63
pazhosch <d.m.baranov@gmail.com>
2019-04-07 15:18:26 +0300

    Revert "Primary (40)"
    This reverts commit 1f714644

--- src/main/java/guru/springframework/DiDemoApplication.java
+++ src/main/java/guru/springframework/DiDemoApplication.java
@@ -16,7 +16,8 @@ public class DiDemoApplication {
 
 		MyController controller = (MyController) ctx.getBean("myController");
 
-		System.out.println(controller.hello());
+		controller.hello();
+
 		System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
 		System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
 		System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
--- src/main/java/guru/springframework/controllers/MyController.java
+++ src/main/java/guru/springframework/controllers/MyController.java
@@ -1,6 +1,5 @@
 package guru.springframework.controllers;
 
-import guru.springframework.services.GreetingService;
 import org.springframework.stereotype.Controller;
 
 /**
@@ -9,15 +8,9 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class MyController {
 
-    private GreetingService greetingService;
-
-    public MyController(GreetingService greetingService) {
-        this.greetingService = greetingService;
-    }
-
     public String hello(){
-        System.out.println(greetingService.sayGreeting());
+        System.out.println("Hello!!! ");
 
-        return greetingService.sayGreeting();
+        return "foo";
     }
 }
